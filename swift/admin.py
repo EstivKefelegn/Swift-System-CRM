e02877e129752e7ecc02069d2d3534c4a9e0e8c2
EstivKefelegn <kefelegnestifanos0@gmail.com>
2025-02-02 08:12:59 +0300

    creating an dmin panel to manage the customers request

--- swift/admin.py
+++ swift/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-
+from . import models
 
 # Register your models here.
+@admin.register(models.SWIFTAccountDetail)
+class SwiftAccountAdmin(admin.ModelAdmin):
+    list_display = ["business_name", "business_address", "account_number"]
+    search_fields = ["business_name", "account_number"]
